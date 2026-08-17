7f4d92b2c8d3c61ba3b365a1984b8d6f98446f4e
sanvinaik <sanvi.naik0103@gmail.com>
2026-08-17 17:31:58 +0530

    ABCD

--- CLI/src/models/tree.cpp
+++ CLI/src/models/tree.cpp
@@ -7,7 +7,8 @@ namespace Models{
     //runs it through the OpenSSL engine, and returns a 64-character readable text string
     //when we add file to the tree, it is already a 64 char hash string which needs to be compressed to 32 bytes
     //so basically, the function will halven the size of the hash string
-    static std::string hexToBinary(const std::string& hex){
+    static std::string hexToBinary(const std::string& hex)
+    {
         if(hex.length()%2 != 0){
             throw std::runtime_error("Malformed hexadecimal string: odd length encountered during tree packaging.");
         }
@@ -15,7 +16,8 @@ namespace Models{
         std::string binaryBytes;
         binaryBytes.reserve(hex.length()/2);
         
-        for(size_t i=0; i<hex.length(); i+=2){
+        for(size_t i=0; i<hex.length(); i+=2)
+        {
             std::string byteString= hex.substr(i, 2);
             //string to integer function using hexadecimal values
             char byte= static_cast<char>(std::stoi(byteString, nullptr, 16));  //converts int of 4 byte to char of 1 byte
@@ -42,16 +44,19 @@ namespace Models{
 }
 
     //adding entry to tree
-    void Tree::addEntry(const TreeDef& entry){
+    void Tree::addEntry(const TreeDef& entry)
+    {
         entries.push_back(entry);
     }
 
     //converts tree object to flat sequence of binary data
-    std::string Tree::serialize() const{
+    std::string Tree::serialize() const
+    {
         std::string treeContent;
 
         //processes every folder or file record entry
-        for(const auto& entry: entries){
+        for(const auto& entry: entries)
+        {
             //[mode] [name]\0[hash_bytes]
             treeContent+= entry.mode+" "+entry.name;  //entry mode info is given below
             treeContent.push_back('\0'); 
